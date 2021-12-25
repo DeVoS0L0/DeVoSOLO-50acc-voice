2005ac9530c21200d80e69847bdb3f3abfbbc218
´《DeVo.SoLo》` <92445216+DeVoS0L0@users.noreply.github.com>
2021-12-25 14:36:53 +0300

    Create devosolo.js

---
+++ devosolo.js
@@ -0,0 +1,355 @@
+const Discord = require("discord.js");
+const devosolo = new Discord.Client();
+const devosolo2 = new Discord.Client();
+const devosolo3 = new Discord.Client();
+const devosolo4 = new Discord.Client();
+const devosolo5 = new Discord.Client();
+const devosolo6 = new Discord.Client();
+const devosolo7 = new Discord.Client();
+const devosolo8 = new Discord.Client();
+const devosolo9 = new Discord.Client();
+const devosolo10 = new Discord.Client();
+const devosolo11 = new Discord.Client();
+const devosolo12 = new Discord.Client();
+const devosolo13 = new Discord.Client();
+const devosolo14 = new Discord.Client();
+const devosolo15 = new Discord.Client();
+const devosolo16 = new Discord.Client();
+const devosolo17 = new Discord.Client();
+const devosolo18 = new Discord.Client();
+const devosolo19 = new Discord.Client();
+const devosolo20 = new Discord.Client();
+const devosolo21 = new Discord.Client();
+const devosolo22 = new Discord.Client();
+const devosolo23 = new Discord.Client();
+const devosolo24 = new Discord.Client();
+const devosolo25 = new Discord.Client();
+const devosolo26 = new Discord.Client();
+const devosolo27 = new Discord.Client();
+const devosolo28 = new Discord.Client();
+const devosolo29 = new Discord.Client();
+const devosolo30 = new Discord.Client();
+const devosolo31 = new Discord.Client();
+const devosolo32 = new Discord.Client();
+const devosolo33 = new Discord.Client();
+const devosolo34 = new Discord.Client();
+const devosolo35 = new Discord.Client();
+const devosolo36 = new Discord.Client();
+const devosolo37 = new Discord.Client();
+const devosolo38 = new Discord.Client();
+const devosolo39 = new Discord.Client();
+const devosolo40 = new Discord.Client();
+const devosolo41 = new Discord.Client();
+const devosolo42 = new Discord.Client();
+const devosolo43 = new Discord.Client();
+const devosolo44 = new Discord.Client();
+const devosolo45 = new Discord.Client();
+const devosolo46 = new Discord.Client();
+const devosolo47 = new Discord.Client();
+const devosolo48 = new Discord.Client();
+const devosolo49 = new Discord.Client();
+const devosolo50 = new Discord.Client();
+
+////////////////////////////////////////////// bas id voice dane
+devosolo.on("ready", () => {
+  var join = devosolo.channels.get("920011397885218816"); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo2.on("ready", () => {
+  var join = devosolo2.channels.get("920011397885218816"); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo3.on("ready", () => {
+  var join = devosolo3.channels.get("920011397885218816"); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo4.on("ready", () => {
+  var join = devosolo4.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo5.on("ready", () => {
+  var join = devosolo5.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo6.on("ready", () => {
+  var join = devosolo6.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo7.on("ready", () => {
+  var join = devosolo7.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo8.on("ready", () => {
+  var join = devosolo8.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo9.on("ready", () => {
+  var join = devosolo9.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo10.on("ready", () => {
+  var join = devosolo10.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo11.on("ready", () => {
+  var join = devosolo11.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo12.on("ready", () => {
+  var join = devosolo12.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo13.on("ready", () => {
+  var join = devosolo13.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo14.on("ready", () => {
+  var join = devosolo14.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo15.on("ready", () => {
+  var join = devosolo15.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo16.on("ready", () => {
+  var join = devosolo16.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo17.on("ready", () => {
+  var join = devosolo17.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo18.on("ready", () => {
+  var join = devosolo18.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo19.on("ready", () => {
+  var join = devosolo19.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo20.on("ready", () => {
+  var join = devosolo20.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo21.on("ready", () => {
+  var join = devosolo21.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo22.on("ready", () => {
+  var join = devosolo22.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo23.on("ready", () => {
+  var join = devosolo23.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo24.on("ready", () => {
+  var join = devosolo24.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo25.on("ready", () => {
+  var join = devosolo25.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo26.on("ready", () => {
+  var join = devosolo26.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo27.on("ready", () => {
+  var join = devosolo27.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo28.on("ready", () => {
+  var join = devosolo28.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo29.on("ready", () => {
+  var join = devosolo29.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo30.on("ready", () => {
+  var join = devosolo30.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo31.on("ready", () => {
+  var join = devosolo31.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo32.on("ready", () => {
+  var join = devosolo32.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo33.on("ready", () => {
+  var join = devosolo33.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo34.on("ready", () => {
+  var join = devosolo34.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo35.on("ready", () => {
+  var join = devosolo35.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo36.on("ready", () => {
+  var join = devosolo36.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo37.on("ready", () => {
+  var join = devosolo37.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo38.on("ready", () => {
+  var join = devosolo38.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo39.on("ready", () => {
+  var join = devosolo39.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo40.on("ready", () => {
+  var join = devosolo40.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo41.on("ready", () => {
+  var join = devosolo41.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo42.on("ready", () => {
+  var join = devosolo42.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo43.on("ready", () => {
+  var join = devosolo43.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo44.on("ready", () => {
+  var join = devosolo44.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo45.on("ready", () => {
+  var join = devosolo45.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo46.on("ready", () => {
+  var join = devosolo46.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo47.on("ready", () => {
+  var join = devosolo47.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo48.on("ready", () => {
+  var join = devosolo48.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo49.on("ready", () => {
+  var join = devosolo49.channels.get(""); /// id voice
+  if (join) join.join();
+});
+////////////////
+devosolo50.on("ready", () => {
+  var join = devosolo50.channels.get(""); /// id voice
+  if (join) join.join();
+});
+
+//////////////// tokn lera da bne
+
+devosolo.login("OTI0MjQxOTY3MTk2NTQ5MTMw.YcbtlA.XZYri5G346XP-0Eadf4gg0-g9I8");
+devosolo2.login("OTI0MjQwNjgwMzYzMDk4MTQy.Ycbsig.gdDiDnZNpQN_rARkYSmChMvo28A");
+devosolo3.login("OTI0MDIzNDQ1NTcyOTUyMDY1.YcbrCQ.hzNwbsBOofhSw6ki0QuTNJmmkrk");
+devosolo4.login("");
+devosolo5.login("");
+devosolo6.login("");
+devosolo7.login("");
+devosolo8.login("");
+devosolo9.login("");
+devosolo10.login("");
+devosolo11.login("");
+devosolo12.login("");
+devosolo13.login("");
+devosolo14.login("");
+devosolo15.login("");
+devosolo16.login("");
+devosolo17.login("");
+devosolo18.login("");
+devosolo19.login("");
+devosolo20.login("");
+devosolo21.login("");
+devosolo22.login("");
+devosolo23.login("");
+devosolo24.login("");
+devosolo25.login("");
+devosolo26.login("");
+devosolo27.login("");
+devosolo28.login("");
+devosolo29.login("");
+devosolo30.login("");
+devosolo31.login("");
+devosolo32.login("");
+devosolo33.login("");
+devosolo34.login("");
+devosolo35.login("");
+devosolo36.login("");
+devosolo37.login("");
+devosolo38.login("");
+devosolo39.login("");
+devosolo40.login("");
+devosolo41.login("");
+devosolo42.login("");
+devosolo43.login("");
+devosolo44.login("");
+devosolo45.login("");
+devosolo46.login("");
+devosolo47.login("");
+devosolo48.login("");
+devosolo49.login("");
+devosolo50.login("");
